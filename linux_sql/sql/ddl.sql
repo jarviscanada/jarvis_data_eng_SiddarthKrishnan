ff715bdc4da98f083bbb88c75d6adf7f54e9fdea
Siddarth Krishnan <siddarthkrishnan.99@live.com>
2021-12-01 21:20:03 +0000

    ddl.sql commands

---
+++ linux_sql/sql/ddl.sql
@@ -0,0 +1,21 @@
+CREATE TABLE IF NOT EXISTS PUBLIC.host_info (
+                                  id    SERIAL PRIMARY KEY,
+                                  hostname   VARCHAR UNIQUE NOT NULL,
+                                  cpu_number INTEGER NOT NULL,
+                                  cpu_architecture VARCHAR NOT NULL,
+                                  cpu_model VARCHAR NOT NULL,
+                                  cpu_mhz REAL NOT NULL,
+                                  L2_cache INTEGER NOT NULL,
+                                  total_mem INTEGER NOT NULL,
+                                  timestamp TIMESTAMP NOT NULL);
+
+CREATE TABLE IF NOT EXISTS PUBLIC.host_usage (
+                                  id    SERIAL PRIMARY KEY,
+                                  hostname   VARCHAR UNIQUE NOT NULL,
+                                  cpu_number INTEGER NOT NULL,
+                                  cpu_architecture VARCHAR NOT NULL,
+                                  cpu_model VARCHAR NOT NULL,
+                                  cpu_mhz REAL NOT NULL,
+                                  L2_cache INTEGER NOT NULL,
+                                  total_mem INTEGER NOT NULL,
+                                  timestamp TIMESTAMP NOT NULL);
