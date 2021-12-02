ce6f87e57a0cb955bff9d9518a92c3268b51cefc
Siddarth Krishnan <siddarthkrishnan.99@live.com>
2021-12-02 22:56:35 +0000

    fix second query for mb kb

--- linux_sql/sql/queries.sql
+++ linux_sql/sql/queries.sql
@@ -1,12 +1,16 @@
-SELECT cpu_number,id,total_mem FROM host_info
+SELECT cpu_number,id,total_mem
+FROM host_info
 GROUP BY cpu_number, id
 ORDER BY cpu_number, total_mem DESC;
 
-select host_id, AVG(host_info.total_mem-host_usage.memory_free), date_trunc('hour', host_usage.timestamp) + date_part('minute', host_usage.timestamp):: int / 5 * interval '5 min'
-from host_usage
-    join host_info
-on host_info.id = host_usage.host_id
-group by host_id, 3;
+SELECT
+    host_id, hostname,
+    (AVG(host_info.total_mem-host_usage.memory_free * 1000)/host_info.total_mem) * 100 as mem_percent_used,
+    round5(host_usage.timestamp)
+FROM host_usage
+         JOIN host_info
+              ON host_info.id = host_usage.host_id
+GROUP BY host_usage.timestamp, host_id, hostname, total_mem;
 
 
 select distinct host_id,
