17844ccb3ae4dfb3591a79ca3a40e465d86bb9e0
Siddarth Krishnan <siddarthkrishnan.99@live.com>
2021-12-13 21:10:27 +0000

    basic configurator for log4j-just testing

--- core_java/grep/src/main/java/ca/jrvs/apps/practice/RegexExcImp.java
+++ core_java/grep/src/main/java/ca/jrvs/apps/practice/RegexExcImp.java
@@ -2,10 +2,20 @@ package ca.jrvs.apps.practice;
 
 import java.util.Locale;
 
+import org.apache.log4j.BasicConfigurator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class RegexExcImp implements RegexExc {
+    private final Logger logger = LoggerFactory.getLogger(RegexExcImp.class);
+    private Boolean t;
+    private Boolean oldT;
 
     public boolean matchJpeg(String filename) {
         filename = filename.toLowerCase();
+        oldT=t;
+        t=filename.matches(".+\\.jp(eg|g)");
+        logger.debug("old return was {} new return is {}", oldT, t);
         return filename.matches(".+\\.jp(eg|g)");
     }
 
@@ -18,7 +28,9 @@ public class RegexExcImp implements RegexExc {
     }
 
     public static void main(String[] args) {
+        BasicConfigurator.configure();
         RegexExcImp regexExcImp = new RegexExcImp();
-        System.out.println(regexExcImp.isEmptyLine(""));
+        regexExcImp.matchJpeg("kek.jps");
+        regexExcImp.logger.info("hello");
     }
 }
