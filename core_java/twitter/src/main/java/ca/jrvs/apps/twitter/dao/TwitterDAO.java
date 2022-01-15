7c6630123eb0866ecf30365a3cfb9b7770c66fc0
Siddarth Krishnan <siddarthkrishnan.99@live.com>
2022-01-15 06:21:44 +0000

    twitterDAO changes

--- core_java/twitter/src/main/java/ca/jrvs/apps/twitter/dao/TwitterDAO.java
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/dao/TwitterDAO.java
@@ -1,11 +1,81 @@
 package ca.jrvs.apps.twitter.dao;
 
+import ca.jrvs.apps.twitter.dao.helper.HttpHelper;
 import ca.jrvs.apps.twitter.model.Tweet;
+import com.google.gdata.util.common.base.PercentEscaper;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.util.EntityUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 
-public class TwitterDAO<T, ID> implements CrdDao<Tweet, String> {
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+
+public class TwitterDAO implements CrdDao<Tweet, String> {
+    //URL constants
+    private static final String API_BASE_URI = "https://api.twitter.com";
+    private static final String POST_PATH = "/1.1/statuses/update.json";
+    private static final String SHOW_PATH = "/1.1/statuses/show.json";
+    private static final String DELETE_PATH = "/1.1/statuses/destroy";
+    //URI symbols
+    private static final String QUERY_SYM = "?";
+    private static final String EQUAL = "=";
+    //response code
+    private static final int HTTP_OK = 200;
+
+    private HttpHelper httpHelper;
+
+    @Autowired
+    public TwitterDAO(HttpHelper httpHelper) {
+        this.httpHelper = httpHelper;
+    }
     @Override
-    public Tweet create(Tweet entity) {
-        return null;
+    public Tweet create(Tweet tweet) {
+        URI uri;
+        try {
+            uri = getPostUri(tweet);
+        } catch (URISyntaxException e) {
+            throw new IllegalArgumentException("Invalid input tweet", e);
+        }
+        HttpResponse httpResponse = httpHelper.httpPost(uri);
+        return new Tweet();
+    }
+
+    private Tweet parseResponseBody(HttpResponse response, Integer expectedStatusCode) {
+        Tweet tweet = null;
+
+        //Check response status
+        int status = response.getStatusLine().getStatusCode();
+        if (status != expectedStatusCode) {
+            try {
+                System.out.println(EntityUtils.toString(response.getEntity()));
+            } catch (IOException e) {
+                System.out.println("Response has no entity");
+            }
+            throw new RuntimeException("unexpected HTTP status:" + status);
+        }
+        if (response.getEntity() == null) {
+            throw new RuntimeException("empty response body");
+        }
+        //convert to JSON for tweet object
+        String body;
+        try {
+            body = EntityUtils.toString(response.getEntity());
+        } catch (IOException e) {
+            throw new RuntimeException("Response has no entity");
+        }
+        try {
+            tweet = JsonUtil.toObjectFromJson(body, Tweet.class);
+        }
+
+    }
+
+    private URI getPostUri(Tweet tweet) throws URISyntaxException {
+        PercentEscaper percentEscaper = new PercentEscaper("", false);
+        return new URI(API_BASE_URI + POST_PATH + QUERY_SYM + "status" + EQUAL + percentEscaper.escape(tweet.getText()));
     }
 
     @Override
--- core_java/twitter/src/test/java/ca/jrvs/apps/twitter/dao/helper/TwitterHttpHelperTest.java
+++ core_java/twitter/src/test/java/ca/jrvs/apps/twitter/dao/helper/TwitterHttpHelperTest.java
@@ -12,10 +12,14 @@ public class TwitterHttpHelperTest {
 
     @Test
     public void httpPost() throws Exception {
-        String consumerKey = System.getenv("consumerKey");
-        String consumerSecret = System.getenv("consumerSecret");
-        String accessToken = System.getenv("accessToken");
-        String tokenSecret = System.getenv("tokenSecret");
+        //String consumerKey = System.getenv("consumerKey");
+        //String consumerSecret = System.getenv("consumerSecret");
+        //String accessToken = System.getenv("accessToken");
+        //String tokenSecret = System.getenv("tokenSecret");
+        String consumerKey = "xgi3jfpp4iWmlqQ2fNf6X1aHo";
+        String consumerSecret = "F7r8hOsvz1dueCoXOaNZFjbKpVrV7F5VMvJqRoi2uoaxeC425l";
+        String accessToken = "3196537057-Ugyjh7eClcynJTX1hIs4rBw2rIhYQ1IxBqMGAV9";
+        String tokenSecret = "lgTeu4l2JkChmiMKjC0hpuNypLcmXbRCRw3pBxS4NUbYI";
         System.out.println(consumerKey + "|" + consumerSecret + "|" + accessToken + "|" + tokenSecret);
         HttpHelper httpHelper = new TwitterHttpHelper(consumerKey, consumerSecret, accessToken, tokenSecret);
         HttpResponse httpResponse = httpHelper.httpPost(new URI("https://api.twitter.com/1.1/statuses/update.json?status=java_testapi_POST"));
@@ -24,13 +28,18 @@ public class TwitterHttpHelperTest {
 
     @Test
     public void httpGet() throws Exception {
-        String consumerKey = System.getenv("consumerKey");
-        String consumerSecret = System.getenv("consumerSecret");
-        String accessToken = System.getenv("accessToken");
-        String tokenSecret = System.getenv("tokenSecret");
+        //String consumerKey = System.getenv("consumerKey");
+        //String consumerSecret = System.getenv("consumerSecret");
+        //String accessToken = System.getenv("accessToken");
+        //String tokenSecret = System.getenv("tokenSecret");
+        String consumerKey = "xgi3jfpp4iWmlqQ2fNf6X1aHo";
+        String consumerSecret = "F7r8hOsvz1dueCoXOaNZFjbKpVrV7F5VMvJqRoi2uoaxeC425l";
+        String accessToken = "3196537057-Ugyjh7eClcynJTX1hIs4rBw2rIhYQ1IxBqMGAV9";
+        String tokenSecret = "lgTeu4l2JkChmiMKjC0hpuNypLcmXbRCRw3pBxS4NUbYI";
         System.out.println(consumerKey + "|" + consumerSecret + "|" + accessToken + "|" + tokenSecret);
         HttpHelper httpHelper = new TwitterHttpHelper(consumerKey, consumerSecret, accessToken, tokenSecret);
         HttpResponse httpResponse = httpHelper.httpGet(new URI("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=sidfromsyd"));
+        //System.out.println(httpResponse.getStatusLine().getStatusCode());
         System.out.println(EntityUtils.toString(httpResponse.getEntity()));
     }
 }
